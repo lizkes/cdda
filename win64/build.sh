395b081f2d389602fe88ef6205a7d4946d8d4714
lizkes <erfan2333@gmail.com>
2021-01-19 19:29:33 +0800

    使用gcc9

--- win64/build.sh
+++ win64/build.sh
@@ -2,5 +2,5 @@ WORK_PATH=`dirname $(readlink -f $0)`
 
 cd ${WORK_PATH}/..
 sed -i "s/lizkes-.*-pure/lizkes-$1-pure/" Makefile
-make -j`nproc` CROSS="${WORK_PATH}/mxe/usr/bin/x86_64-w64-mingw32.static-" BUILD_PREFIX="./win64/" \
+make -j`nproc` CROSS="${WORK_PATH}/mxe/usr/bin/x86_64-w64-mingw32.static.gcc9-" BUILD_PREFIX="./win64/" \
     CCACHE=0 TILES=1 SOUND=1 RELEASE=1 LOCALIZE=1 LANGUAGES=all LIBBACKTRACE=0 bindist
