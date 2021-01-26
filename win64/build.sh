52326c3eab0e887551393f83209610362080006b
lizkes <erfan2333@gmail.com>
2021-01-26 14:27:28 +0800

    将pure改成custom

--- win64/build.sh
+++ win64/build.sh
@@ -1,6 +1,6 @@
 WORK_PATH=`dirname $(readlink -f $0)`
 
 cd ${WORK_PATH}/..
-sed -i "s/VERSION = lizkes-*/VERSION = lizkes-$1-pure/" Makefile_lizkes
+sed -i "s/VERSION = lizkes-*/VERSION = lizkes-$1-custom/" Makefile_lizkes
 make -j`nproc` --file=Makefile_lizkes CROSS="${WORK_PATH}/mxe/usr/bin/x86_64-w64-mingw32.static.gcc9-" \
 BUILD_PREFIX="win64/" CCACHE=0 TILES=1 SOUND=1 RELEASE=1 LOCALIZE=1 LANGUAGES=all LIBBACKTRACE=0 bindist
